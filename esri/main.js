6f8fb1ef71a81e6a0bb6d6f89361a084e55a25c2
Tim Keane <t.d.keane@gmail.com>
2023-09-11 13:33:28 -0400

    vit-config

--- esri/main.js
+++ esri/main.js
@@ -1,8 +1,14 @@
 import Map from '@arcgis/core/Map';
-import View from '@arcgis/core/views/MapView';
+import MapView from '@arcgis/core/views/MapView';
 import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer';
 import Graphic from '@arcgis/core/Graphic.js';
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import proj4 from 'proj4';
+
+proj4.defs([
+  ['EPSG:2263', '+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000.0000000001 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=ft +to_meter=0.3048006096012192 +no_defs'],
+  ['EPSG:6539', '+proj=lcc +lat_1=40.66666666666666 +lat_2=41.03333333333333 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000 +y_0=0 +ellps=GRS80 +units=us-ft +no_defs']
+]);
 
 const pole = proj4('EPSG:2263', 'EPSG:4326', [994949, 149776]);
 const point = {
