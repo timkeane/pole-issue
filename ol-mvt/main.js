626b831900a569bd27a05a1280b9f460ebc4ac99
Tim Keane <t.d.keane@gmail.com>
2023-09-12 09:26:17 -0400

    sidewalk

--- ol-mvt/main.js
+++ ol-mvt/main.js
@@ -6,11 +6,16 @@ import Layer from 'ol/layer/Vector';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import proj4 from 'proj4';
+import {register} from 'ol/proj/proj4';
+import GeoJSON from 'ol/format/GeoJSON';
+import Style from 'ol/style/Style';
+import Stroke from 'ol/style/Stroke';
 
 proj4.defs([
   ['EPSG:2263', '+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000.0000000001 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=ft +to_meter=0.3048006096012192 +no_defs'],
   ['EPSG:6539', '+proj=lcc +lat_1=40.66666666666666 +lat_2=41.03333333333333 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000 +y_0=0 +ellps=GRS80 +units=us-ft +no_defs']
 ]);
+register(proj4);
 
 const pole = proj4('EPSG:2263', 'EPSG:3857', [994949, 149776]);
 const view = new View({
@@ -22,10 +27,29 @@ const source = new Source({});
 const layer = new Layer({source});
 source.addFeature(feature);
 
+const sidewalk = new Layer({
+  source: new Source({
+    format: new GeoJSON({
+      dataProjection: 'EPSG:2263',
+      featureProjection: 'EPSG:3857'
+    
+    }),
+    url: './sidewalk.json'
+  }),
+  style: new Style({
+    stroke: new Stroke({
+      width: 1,
+      lineDash: [5, 5],
+      color: 'black'
+    })
+  })
+});
+
 olms(
   'map',
   'https://www.arcgis.com/sharing/rest/content/items/2ee3ac7f481548c88d53ea50268525e7/resources/styles/root.json?f=json'
 ).then(function (map) {
   map.setView(view);
+  map.addLayer(sidewalk);
   map.addLayer(layer);
 });
